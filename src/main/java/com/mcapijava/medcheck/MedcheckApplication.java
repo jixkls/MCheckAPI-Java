22a02ada8302400a2c71bb870d775f2f59497271
Jixkls <bettindiogo@gmail.com>
2025-11-10 00:54:26 -0300

    Setup And Health Check for the Application

---
+++ src/main/java/com/mcapijava/medcheck/MedcheckApplication.java
@@ -0,0 +1,13 @@
+package com.mcapijava.medcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedcheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedcheckApplication.class, args);
+	}
+
+}
