dafec596365e0cd6f1ca2a4b95ce440f306bfdfb
Jixkls <bettindiogo@gmail.com>
2025-11-17 20:40:18 -0300

    Adicionando Dummys de exemplo nas tables e configurando o application.properties

---
+++ src/main/resources/db/migration/V2__insert_dummy_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO cities (id, name, state) VALUES
+('11111111-1111-1111-1111-111111111111', 'São Paulo',      'SP'),
+('22222222-2222-2222-2222-222222222222', 'Apucarana',      'PR'),
+('33333333-3333-3333-3333-333333333333', 'Rio de Janeiro', 'RJ');
+
+INSERT INTO specialties (id, name) VALUES
+('aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'Cardiologia'),
+('bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb', 'Dermatologia'),
+('cccccccc-cccc-cccc-cccc-cccccccccccc', 'Pediatria'),
+('dddddddd-dddd-dddd-dddd-dddddddddddd', 'Clínica Geral');
